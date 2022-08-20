fb6bf099837205952224978097d3d0f5e07a271c
junaaid96 <info.junaidulislam@gmail.com>
2022-08-21 01:29:59 +0600

    some bugs fixed

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,20 +11,41 @@ const withdrawField = document.getElementById('withdraw-area');
 document.getElementById('deposit-btn').addEventListener('click', function make_deposit() {
     let initDepo = parseInt(depositField.value);
     let lastDepo = parseInt(deposit.innerText) + initDepo;
-    deposit.innerText = lastDepo;
+    
     let initBal = parseInt(depositField.value);
     let lastBal = parseInt(balance.innerText) + initBal;
+    
+    depositField.value = '';
+
+    if (initDepo < 0) {
+        alert('Enter a positive Number!');
+        return; //stop function
+    }
+
+    if(isNaN(lastBal)){
+        alert('Enter a valid number');
+        return;
+    }
+    deposit.innerText = lastDepo;
     balance.innerText = lastBal;
 })
 
 document.getElementById('withdraw-btn').addEventListener('click', function make_withdraw() {
     let initWithd = parseInt(withdrawField.value);
-    let lastWithd = parseInt(withdraw.innerText)+initWithd;
-    withdraw.innerText = lastWithd;
     let lastBal = parseInt(balance.innerText) - initWithd;
+    let lastWithd = parseInt(withdraw.innerText)+initWithd;
+    withdrawField.value = ''; // clear input
+
     if (lastBal < 0) {
         alert('Insufficient Balance!');
-    } else {
-        balance.innerText = lastBal;
+        return; //stop function
     }
+    //write these lines after if-condition so that they wont affected before function stop
+
+    if(isNaN(lastBal)){
+        alert('Enter a valid number');
+        return;
+    }
+    withdraw.innerText = lastWithd;
+    balance.innerText = lastBal;
 })
